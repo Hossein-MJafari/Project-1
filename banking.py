0a7b6acd54b3831d1ebbc98b041048be75f41780
Hossein MJafari <ho3ein.mj.1@gmail.com>
2023-04-08 08:42:21 +0430

    Bank module modified.
    created the validator to check if the card number is 16 digits or not and save the result in the payment_result.txt

--- banking.py
+++ banking.py
@@ -1,2 +1,19 @@
-class Validation:
-    pass
+
+def is_16_digits(card_number, full_name):
+    print("Validating your card number...")
+    if len(str(card_number)) == 16:
+        with open('payment_result.txt', 'a') as f:
+            f.write(f'{card_number}\nSuccessful Payment...\n{full_name}\n')
+        print("Your payment was successful, the payment factor has been created.\nPlease close this window to continue...")
+    else:
+        with open('payment_result.txt', 'a') as f:
+            f.write(
+                f'{card_number}\nUnsuccessful Payment, Try Again...\n{full_name}\n')
+        print("Your payment was unsuccessful, the payment factor has been created.\nPlease close this window to continue...")
+
+
+class Validator:
+    def __init__(self, card_number, full_name):
+        self.__card_number = card_number
+        self.__full_name = full_name
+        is_16_digits(self.__card_number, self.__full_name)
