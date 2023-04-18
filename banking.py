847af3471f5724adb6ee04332dfee371361ea6ca
mohamadrezaRaouf <130048885+mohamadrezaRaouf@users.noreply.github.com>
2023-04-18 10:59:27 +0430

    updating banking process

--- banking.py
+++ banking.py
@@ -1,19 +1,53 @@
+from order import Payment_data , Cart , Factor
+from logistic import Time
+import csv
 
-def is_16_digits(card_number, full_name):
-    print("Validating your card number...")
-    if len(str(card_number)) == 16:
-        with open('payment_result.txt', 'a') as f:
-            f.write(f'{card_number}\nSuccessful Payment...\n{full_name}\n')
-        print("Your payment was successful, the payment factor has been created.\nPlease close this window to continue...")
-    else:
-        with open('payment_result.txt', 'a') as f:
-            f.write(
-                f'{card_number}\nUnsuccessful Payment, Try Again...\n{full_name}\n')
-        print("Your payment was unsuccessful, the payment factor has been created.\nPlease close this window to continue...")
+with open("orders.csv", "w") as file:
+        writer=csv.writer(file)
+        writer.writerow(['quantity','order_number','pure_prize','sent_prize','tax'])
+class Banking(Payment_data):
+    def cheking_card (self):
+        super().make_payment()
+        if self.result=='Payment was successful!' :
+            #shomare sefaresh to ghesmate factor
+            #gheymato havaset bashe
+             writer.writerow([Cart.cart[product_name][size],Factor.order_number,Factor.pure_prize,Time.prize,Factor.pure_prize * 0.09])
+            
+        else :
+             pass
+with open('orders.csv','r') as file:
+     reader=csv.reader(file)
+class Output:
+     def output(self):
+          return reader
+          
+          
 
 
-class Validator:
-    def __init__(self, card_number, full_name):
-        self.__card_number = card_number
-        self.__full_name = full_name
-        is_16_digits(self.__card_number, self.__full_name)
+
+
+
+
+
+
+
+
+
+#def is_16_digits(card_number, full_name):
+ #   print("Validating your card number...")
+  #  if len(str(card_number)) == 16:
+   #     with open('payment_result.txt', 'a') as f:
+    #        f.write(f'{card_number}\nSuccessful Payment...\n{full_name}\n')
+     #   print("Your payment was successful, the payment factor has been created.\nPlease close this window to continue...")
+    #else:
+     #   with open('payment_result.txt', 'a') as f:
+      #      f.write(
+       #         f'{card_number}\nUnsuccessful Payment, Try Again...\n{full_name}\n')
+       # print("Your payment was unsuccessful, the payment factor has been created.\nPlease close this window to continue...")
+
+
+#class Validator:
+ #   def __init__(self, card_number, full_name):
+  #      self.__card_number = card_number
+   #     self.__full_name = full_name
+
