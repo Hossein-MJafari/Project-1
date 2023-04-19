925878572df440bdece674eb192b6344fb27353a
mohamadrezaRaouf <130048885+mohamadrezaRaouf@users.noreply.github.com>
2023-04-19 13:06:07 +0430

    .

--- main.py
+++ main.py
@@ -5,65 +5,65 @@ from wearhouse import Manual_Update
 from banking import Output
 if __name__ == '__main__':
 
-class Start(Manual_Update):
+ class Start(Manual_Update):
    
-    def start(self):
-        print("Account mode: \n 1.customer \n 2.seller")
-        acount_mode = int (input("Enter your choice: "))
+     def start(self):
+         print("Account mode: \n 1.customer \n 2.seller")
+         acount_mode = int (input("Enter your choice: "))
+ 
+         if acount_mode == 1:
+             while True:
+                 print("Customer Mode Menu:")
+                 print("1.show available products")
+                 print("2.show your cart")
+                 print("3.payment")
+                 print("0.exit customer mode")
+                 customer_menu = int(input("Enter your choice: "))
+                 if customer_menu == 1 :
+                     return pd.read_csv("main wearhouse.csv")
+                 elif customer_menu == 2:
+                     return Cart.cart
+                 elif customer_menu == 3:
+                     return Payment_data
+                 elif customer_menu == 0:
+                     break
+                 else:
+                     print("Invalid choice! Try again...")
 
-        if acount_mode == 1:
-            while True:
-                print("Customer Mode Menu:")
-                print("1.show available products")
-                print("2.show your cart")
-                print("3.payment")
-                print("0.exit customer mode")
-                customer_menu = int(input("Enter your choice: "))
-                if customer_menu == 1 :
-                    return pd.read_csv("main wearhouse.csv")
-                elif customer_menu == 2:
-                    return Cart.cart
-                elif customer_menu == 3:
-                    return Payment_data
-                elif customer_menu == 0:
-                    break
-                else:
-                    print("Invalid choice! Try again...")
+         elif acount_mode == 2:
+             while True:
+                 print("Seller Mode Menu:")
+                 print("1.Wearhouse")
+                 print("2.Banking")
+                 print("0.Exit seller mode")
+                 seller_menu = int(input("Enter your choice: "))
+                 if seller_menu == 1:
+                     super().__init__()
+                     print('we have 2 options to update wear house: ')
+                     print('1:giving csv')
+                     print('2:giving informations')
+                     seller_choice=int(input('Enter your choice:'))
 
-        elif acount_mode == 2:
-            while True:
-                print("Seller Mode Menu:")
-                print("1.Wearhouse")
-                print("2.Banking")
-                print("0.Exit seller mode")
-                seller_menu = int(input("Enter your choice: "))
-                if seller_menu == 1:
-                    super().__init__()
-                    print('we have 2 options to update wear house: ')
-                    print('1:giving csv')
-                    print('2:giving informations')
-                    seller_choice=int(input('Enter your choice:'))
-
-                    if seller_choice==1:
-                        super().update_stock_from_csv()
-                        #return Manual_Update.update_stock_from_csv()
+                     if seller_choice==1:
+                         super().update_stock_from_csv()
+                         #return Manual_Update.update_stock_from_csv()
                     
-                    elif seller_choice==2:
-                        super().update_stock_from_terminal()
-                        #return Manual_Update.update_stock_from_terminal()
+                     elif seller_choice==2:
+                         super().update_stock_from_terminal()
+                         #return Manual_Update.update_stock_from_terminal()
 
-                    else:
-                        print("Invalid choice! Try again...")
+                     else:
+                         print("Invalid choice! Try again...")
 
 
-                elif seller_menu == 2:
-                    return Output
-                
-                elif seller_menu == 0:
-                    break
-                
-                else:
-                    print("Invalid choice! Try again...")
-
-        else:
-            print("Invalid choice!")
+                 elif seller_menu == 2:
+                     return Output
+                 
+                 elif seller_menu == 0:
+                     break
+ 
+                 else:
+                     print("Invalid choice! Try again...")
+ 
+         else:
+             print("Invalid choice!")
